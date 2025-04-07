9382197409fd239bfc84a41c8677221ae8bf33f7
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-04-06 23:24:15 -0300

    feat: implement jwt authentication filter

--- src/main/java/com/erico/accessmanagement/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/erico/accessmanagement/auth/filter/JwtAuthenticationFilter.java
@@ -1,19 +1,57 @@
 package com.erico.accessmanagement.auth.filter;
 
+import com.erico.accessmanagement.auth.service.CustomUserDetailsService;
+import com.erico.accessmanagement.auth.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractJwtFromHeader(request);
+
+        if (token != null) {
+            String subject = jwtService.getSubject(token);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
+
+            var authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
         filterChain.doFilter(request, response);
+    }
+
+    private String extractJwtFromHeader(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (token != null) {
+            return token.replace("Bearer", "").trim();
+        }
+        return null;
     }
 }
