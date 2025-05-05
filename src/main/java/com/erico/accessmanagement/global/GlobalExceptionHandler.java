7d1a9b6a7b082d53c6c03044ecbcfe68f9eeb9d8
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-05-05 18:46:38 -0300

    feat: add handleUnauthorized method

--- src/main/java/com/erico/accessmanagement/global/GlobalExceptionHandler.java
+++ src/main/java/com/erico/accessmanagement/global/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ import com.erico.accessmanagement.dto.ErrorResponseDto;
 import com.erico.accessmanagement.dto.FieldErrorsResponseDto;
 import com.erico.accessmanagement.exception.ResourceConflictException;
 import com.erico.accessmanagement.exception.ResourceGoneException;
+import com.erico.accessmanagement.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Set;
@@ -16,24 +20,35 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ResourceConflictException.class)
     public ResponseEntity<ErrorResponseDto> handleConflict(Exception ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(new ErrorResponseDto(ex.getMessage()));
     }
 
+    @ResponseStatus(HttpStatus.GONE)
     @ExceptionHandler(ResourceGoneException.class)
     public ResponseEntity<ErrorResponseDto> handleGone(Exception ex) {
         return ResponseEntity.status(HttpStatus.GONE)
                 .body(new ErrorResponseDto(ex.getMessage()));
     }
 
-    @ExceptionHandler(ResourceGoneException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleNotFound(Exception ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponseDto(ex.getMessage()));
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDto> handleUnauthorized(Exception ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDto(ex.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Set<FieldErrorsResponseDto>> handleUnprocessableEntity(MethodArgumentNotValidException ex) {
         Set<FieldErrorsResponseDto> erros = ex.getBindingResult()
